d2bd9fdba8d140b22e1deecd068a1aa5ca8f8b8b
Omkar Jaree <omkarjarepatil512@gmail.com>
2024-10-01 11:34:22 +0530

    installed react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  // console.log("")
+  return (
+    <>
+    <h1 className='text-red-600'>App</h1>
+    <h1>sdasd</h1>
+    </>
+  )
+}
