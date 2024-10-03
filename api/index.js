0526480f70aeae3449dcdbb0f275d9710fb27ac7
Omkar Jaree <omkarjarepatil512@gmail.com>
2024-10-03 18:44:23 +0530

    Created user model and atest api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { MongoClient } from 'mongodb';
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 
@@ -18,3 +18,5 @@ app.listen(3000, () => {
     console.log("server is running on 3000 port !!")
     }
 );
+
+app.use("/api/user", userRouter)
---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import  Mongoose  from "mongoose";
+
+const userSchema = new Mongoose.Schema({
+    usernames: {
+        type : 'string',
+        required: true,
+        unique: true,
+    },
+    email: {
+        type : 'string',
+        required: true,
+        unique: true,
+    },
+    password: {
+        type : 'string',
+        required: true,
+    }
+}, { Timestamp: true });
+
+const User = Mongoose.model('User', userSchema);
+
+export default User;
