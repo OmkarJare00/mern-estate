3cf6c9586bd7da142dc9b22f926108d0559faa88
Omkar Jaree <omkarjarepatil512@gmail.com>
2024-10-18 23:50:16 +0530

    created signup functionality and signin route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 // import router from './user.route';
-import { singup } from '../controllers/auth.controller.js';
+import { signin, singup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup",singup)
+router.post("/signin",signin)
 
 export default router;
